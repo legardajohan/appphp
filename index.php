227093e4a27d82dcbcd6a90f6c2c058859054ed7
Johan Legarda <legardajohan@gmail.com>
2021-09-29 16:56:10 -0500

    Operadores aritmeticos, variables y constantes

--- index.php
+++ index.php
@@ -11,6 +11,37 @@ $jobis = [
     'sunday' => 'play the guitar'
 ]; 
 
+// echo $name; 
+echo 'Hola! Mi nombre es ' . $name . ' y tengo ' . $age . ' años <br>';
+
+// Operadores aritmeticos 
+
+// echo '2 + 3 = ' . (2+3);
+// echo '<br> 3 * 5 = ' . (3*5);
+
+$a = 30;
+$b = 15; 
+
+echo '30 + 15 = ' . ($a + $b);
+echo '<br> 30 * 15 = ' . ($a * $b);
+echo '<br> 30 /15 = ' . ($a / $b);
+echo '<br> 30 - 15 = ' . ($a - $b);
+echo '<br> 5 % 3 = ' . (5 % 3); 
+echo '<br> 2 ** 3 = ' . (2 ** 3); 
+
+define ('curso', 'AppWeb en Servidor');
+define ('password', 12345);
+
+echo '<br> Mi constante es: ' . (curso);
+echo '<br> La contraseña es: ' . (password);
+
+
+
+// Igual ==, valor '9' == 9 
+// Igual ===, valor - tipo 9 === 9
+// Diferencias !=, valor 
+// Diferencias !==, valor - tipo
+// <, >, <=, >=
 
 
 ?>
